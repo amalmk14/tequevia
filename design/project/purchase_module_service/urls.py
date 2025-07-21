ebcc1539f7d5effdda2ab810855603d9846d667e
Tequevia <tequevia@gmail.com>
2025-07-21 15:46:28 +0530

    ...

--- design/project/purchase_module_service/urls.py
+++ design/project/purchase_module_service/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
-from rest_framework.router import DefaultRouter
-from .views import *
+from rest_framework.routers import DefaultRouter
+from .views import * 
 
 router = DefaultRouter()
 
@@ -10,40 +10,4 @@ router.register(r'level3categories',Level3CategoryViewsets)
 
 urlpatterns = [
     path('',include(router.urls))
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# urls.py
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, Level2CategoryViewSet, Level3CategoryViewSet
-
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'level2-categories', Level2CategoryViewSet)
-router.register(r'level3-categories', Level3CategoryViewSet)
-
-urlpatterns = [
-    path('api/', include(router.urls)),
 ]
--- design/project/purchase_module_service/views.py
+++ design/project/purchase_module_service/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from rest_framewrok import viewsets
+from rest_framework import viewsets
 from .models import *
 from .serializers import *
 # Create your views here.
 
-class CategoryViewsets(viewsets.ModelViewsets):
+class CategoryViewsets(viewsets.ModelViewSet):
     queryset = Category.objects.filter(delete_status=True).order_by('-created_on')
     serializer_class = CategorySerializer
 
-class Level2CategoryViewsets(viewsets.ModelViewsets):
+class Level2CategoryViewsets(viewsets.ModelViewSet):
     queryset = Level2Category.objects.filter(delete_status=True).order_by('-created_on')
     serializer_class = Level2Serializer
 
-class Level3CategoryViewsets(viewsets.ModelViewsets):
+class Level3CategoryViewsets(viewsets.ModelViewSet):
     queryset = Level3Category.objects.filter(delete_status=True).order_by('-created_on')
     serializer_class = level3Serializer
 
