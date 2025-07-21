61d7a7b4306a16e643fcfe3a767b94ecf36b554d
Tequevia <tequevia@gmail.com>
2025-07-21 16:33:06 +0530

    ...

--- design/project/purchase_module_service/serializers.py
+++ design/project/purchase_module_service/serializers.py
@@ -11,7 +11,41 @@ class Level2Serializer(serializers.ModelSerializer):
         model = Level2Category
         fields = '__all__'
 
-class level3Serializer(serializers.ModelSerializer):
+class Level3Serializer(serializers.ModelSerializer):
     class Meta:
         model = Level3Category
         fields = '__all__'
+
+class Sizeserializer(serializers.ModelSerializer):
+    class Meta:
+        model = Size
+        fields = "__all__"
+
+class MaterialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Material
+        fields = "__all__"
+
+class CollarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collar
+        fields = "__all__"
+
+class NeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Neck
+        fields = "__all__"
+
+class SleeveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sleeve
+        fields = "__all__"
+
+class BadgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Badge
+        fields = "__all__"
+        
+                
+        
+        
--- design/project/purchase_module_service/views.py
+++ design/project/purchase_module_service/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import *
 from .serializers import *
 # Create your views here.
 
 class CategoryViewsets(viewsets.ModelViewSet):
-    queryset = Category.objects.filter(delete_status=True).order_by('-created_on')
+    queryset = Category.objects.filter(delete_status=False).order_by('-created_on')
     serializer_class = CategorySerializer
 
+    filter_backend = [filters.SearchFilter]
+    search_filter = ['category_name']
+
 class Level2CategoryViewsets(viewsets.ModelViewSet):
-    queryset = Level2Category.objects.filter(delete_status=True).order_by('-created_on')
+    queryset = Level2Category.objects.filter(delete_status=False).order_by('-created_on')
     serializer_class = Level2Serializer
 
 class Level3CategoryViewsets(viewsets.ModelViewSet):
-    queryset = Level3Category.objects.filter(delete_status=True).order_by('-created_on')
-    serializer_class = level3Serializer
+    queryset = Level3Category.objects.filter(delete_status=False).order_by('-created_on')
+    serializer_class = Level3Serializer
 
 
 
