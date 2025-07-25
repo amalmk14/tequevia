11fa989b9ea1ab2771a534b89820546e2db2dc9d
Tequevia <tequevia@gmail.com>
2025-07-25 10:01:21 +0530

    ...

--- design/project/purchase_module_service/urls.py
+++ design/project/purchase_module_service/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
     # path('signout/',SignoutView.as_view(), name="signout"),
     # # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
     # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+
 ]
 
